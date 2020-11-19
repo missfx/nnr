e6a0cd65c4e5b9794de0ec46a2c76f1f537a62c6
zehelh <yvan.brai@gmail.com>
2020-11-19 16:24:20 +0100

    v1 preprod
    v1 preprod

---
+++ wp-content/themes/nnr/footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package gutenberg-starter-theme
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="contentFooter fullGrid">
+			<div class="grid80">
+				<h2>Nantes Numérique Responsable</h2>
+				<p>Collectif nantais pour la promotion d’un numérique éthique, accessible, inclusif & durable, fondé en 2020.</p>
+			</div>
+			<div class="grid20">
+				<ul>
+					<li>
+						<a href="mailto:contact@nantes-numerique-responsable.org">→ Email</a>
+					</li>
+					<li>
+						<a href="https://twitter.com/NantesNR" target="_blank">→ Twitter</a>
+					</li>
+					<li>
+						<a href="https://www.meetup.com/fr-FR/nantesnr/" target="_blank">→ Meetup</a>
+					</li>
+				</ul>
+			</div>
+			<div class="copyright grid100">Nantes Numérique Responsable 2020 - Licence Creative Commons</div>
+		</div><!-- .contentFooter -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
