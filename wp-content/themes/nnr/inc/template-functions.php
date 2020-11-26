ebd33685e751c2e1469000ccb69147a886768b72
Maxouleloup <maxence_86@hotmail.fr>
2020-11-26 15:53:48 +0100

    Update template-functions.php

--- wp-content/themes/nnr/inc/template-functions.php
+++ wp-content/themes/nnr/inc/template-functions.php
@@ -30,3 +30,52 @@ function gutenberg_starter_theme_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'gutenberg_starter_theme_pingback_header' );
+
+add_action( ‘init’, ‘register_my_cpt_membres’, 10 );
+function register_my_cpt_membres() {
+register_post_type( “membres”, array (
+    ‘labels’ =>
+    array (
+    ‘name’ => ‘membres’,
+    ‘singular_name’ => ‘membre’,
+    ‘add_new’ => ‘Ajouter’,
+    ‘add_new_item’ => ‘Ajouter une nouvelle membre’,
+    ‘edit_item’ => ‘Modifier le membre’,
+    ‘new_item’ => ‘Nouvelle membre’,
+    ‘view_item’ => ‘Voir l membre’,
+    ‘search_items’ => ‘Chercher un membre’,
+    ‘not_found’ => ‘Aucun membre trouvé’,
+    ‘not_found_in_trash’ => ‘Aucun membre trouvé dans la corbeille’,
+    ‘parent_item_colon’ => ‘membre parent:’,
+    ),
+    ‘description’ => ‘’,
+    ‘publicly_queryable’ => true,
+    ‘exclude_from_search’ => false,
+    ‘map_meta_cap’ => true,
+    ‘capability_type’ => ‘post’,
+    ‘public’ => true,
+    ‘hierarchical’ => false,
+    ‘rewrite’ =>
+    array (
+        ‘slug’ => ‘liste-membres’,
+        ‘with_front’ => true,
+        ‘pages’ => true,
+        ‘feeds’ => true,
+    ),
+    ‘has_archive’ => true,
+    ‘query_var’ => ‘membres’,
+    ‘supports’ =>
+    array (
+        0 => ‘title’,
+        1 => ‘editor’,
+        3 => ‘thumbnail’
+    ),
+    ‘show_ui’ => true,
+    ‘menu_position’ => 30,
+    ‘menu_icon’ => false,
+    ‘can_export’ => true,
+    ‘show_in_nav_menus’ => true,
+    ‘show_in_menu’ => true,
+    )
+    );
+}
