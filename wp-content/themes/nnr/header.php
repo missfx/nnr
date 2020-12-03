35cf950d7884d6bdb73bce26239745a65d9e9abf
zehelh <yvan.brai@gmail.com>
2020-12-03 13:48:33 +0100

    maj 03/12 add favicon
    favicon added

--- wp-content/themes/nnr/header.php
+++ wp-content/themes/nnr/header.php
@@ -15,6 +15,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style-404.css">
+	<link rel="icon" type="image/png" href="favicon.png" />
 
 	<?php wp_head(); ?>
 </head>
