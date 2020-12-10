6a2f59831b1614d401ec004f5dcbfb49db24a898
Charly <charly2221@hotmail.fr>
2020-12-10 14:32:02 +0100

    Ajout d'un template part pour la page actualite

--- wp-content/themes/nnr/index.php
+++ wp-content/themes/nnr/index.php
@@ -14,52 +14,10 @@
 
 get_header(); ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-	<?php $sticky = get_option('sticky_posts');
-	$args = array(
-			'posts_per_page' => 1,
-			'post__in' => $sticky,
-			'ignore_sticky_posts' => 1
-	);
-	$posts = new WP_Query($args);
-	if (isset($sticky[0])) {
-		?>
-	<h2><?= $posts->posts[0]->post_title ?></h2>
-	<p><?= $posts->posts[0]->post_content ?></p>
-	<?php
-	}
-
-	$args = array(
-			'posts_per_page' => 4,
-			'post__not_in' => $sticky,
-			"cat" => 1,
-
-	);
-
-	$query = new WP_Query($args);
-		while ($query -> have_posts()) :
-			$query->the_post();
-	?>
-	<h2><?=the_title()?></h2>
-	<span>Posté le <?= get_the_date(); ?>, par <?= get_the_author(); ?></span>
-	<p> <?= the_excerpt(); ?> </p>
-	<a href="<?=the_permalink()?>" rel="bookmark">Lire la suite</a>
-
-<?php
-       
-    endwhile;
-    wp_reset_postdata();
-    $args = array(
-        'post__not_in' => $sticky,
-        "cat" => 1,
-
-    );
-    $query = new WP_Query($args);
-
-?>
-
-	</main><!-- #primary -->
+    <?php get_template_part('template-parts/content', 'actualite'); ?>
+</main><!-- #primary -->
 
 <?php
 get_footer();
