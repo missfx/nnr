ad56891db1409415c4a584dfb386f63220942031
Charly <charly2221@hotmail.fr>
2020-12-10 15:36:56 +0100

    Récupération des changements et alignement VSCODE

--- wp-content/themes/nnr/header.php
+++ wp-content/themes/nnr/header.php
@@ -11,34 +11,29 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style-404.css">
-<<<<<<< HEAD
-	<link rel="icon" type="image/png" href=" <?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
-=======
-	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style-article.css">
->>>>>>> 101e3b1402a5c996cd0fb32c0fff0c23b5337e48
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style-404.css">
+        <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style-article.css">
 
-	<?php wp_head(); ?>
-</head>
+        <?php wp_head(); ?>
+    </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				NNR
-			</div>
+    <body <?php body_class(); ?>>
+        <div id="page" class="site">
+            <header id="masthead" class="site-header">
+                <div class="site-branding">
+                    NNR
+                </div>
 
-			<nav id="site-navigation" class="main-navigation">
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					) );
-				?>
-			</nav>
-		</header>
-
+                <nav id="site-navigation" class="main-navigation">
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                    ));
+                ?>
+                </nav>
+            </header>
